d65ca30dd59739390c7bdf095349bd2bb9032968
TakumiNishino <nistak04@yahoo.co.jp>
2017-08-17 18:07:34 +0900

    表示周りを実装

--- main.c
+++ main.c
@@ -44,6 +44,232 @@
 
 int checkgit = 460;
 
+char map[18][18];
+
+void show(char, char);
+void setup();
+void mapcopy(char, char);
+//void trans(char);
+void makefig(char, char, char, char);
+void posh(char, char, int, char);
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    setup();
+    for (char i = 0; i < 18; i++) {
+        for (char j = 0; j < 18; j++) {
+            map[i][j] = 0;
+        }
+    }
+
+    int figure = 0;
+
+    int count = 0;
+
+    while (1) {
+        show(0, 0);
+        posh(1,1,460,1);
+    }
+    return (EXIT_SUCCESS);
+}
+
+void setup() {
+    OSCCON = 0b01110000;
+    ANSELA = 0x00;
+    ANSELB = 0x00;
+    TRISA = 0x00;
+    TRISB = 0x00;
+    TRISC = 0x00;
+    TRISD = 0x00;
+    TRISE = 0x00;
+}
+
+void show(char num1, char num2) {
+    LATD = 0x00;
+    LATA = 0x00;
+    LATE2 = 0;
+    LATE1 = 1;
+    LATE1 = 0;
+    mapcopy(num1, num2);
+    __delay_us(100);
+    LATE2 = 1;
+    for (char i = 1; i < 16; i++) {
+        LATD = 0x00;
+        LATA = 0x00;
+        LATE1 = 1;
+        LATE1 = 0;
+        mapcopy(num1, num2 + i);
+        __delay_us(100);
+    }
+}
+
+void mapcopy(char num1, char num2) {
+    char datatempa = 0;
+    char datatemp = 0;
+    datatemp = map[num1 + 7][num2];
+    datatempa = map[num1 + 15][num2];
+    for (char i = 7; i > 0; i--) {
+        datatemp = datatemp * 2 + map[num1 + i - 1][num2];
+        datatempa = datatempa * 2 + map[num1 + i + 7][num2];
+    }
+
+    LATD = datatemp;
+    LATA = datatempa;
+}
+
+void posh(char num1, char num2, int point, char mode) {
+    if (point > 99)makefig(num1, num2, point / 100, mode);
+    if (point > 9)makefig(num1, num2 + 4, (point % 100) / 10, mode);
+    makefig(num1, num2 + 8, point % 10, mode);
+}
+
+void makefig(char num1, char num2, char figure, char mode) {
+    switch (figure) {
+        case 1:
+            map[num1][num2 + 1] = mode;
+            map[num1 + 1][num2 + 1] = mode;
+            map[num1 + 2][num2 + 1] = mode;
+            map[num1 + 3][num2 + 1] = mode;
+            map[num1 + 4][num2 + 1] = mode;
+            break;
+        case 2:
+            map[num1][num2] = mode;
+            map[num1][num2 + 1] = mode;
+            map[num1][num2 + 2] = mode;
+            map[num1 + 1][num2 + 2] = mode;
+            map[num1 + 2][num2 + 2] = mode;
+            map[num1 + 2][num2 + 1] = mode;
+            map[num1 + 2][num2] = mode;
+            map[num1 + 3][num2] = mode;
+            map[num1 + 4][num2] = mode;
+            map[num1 + 4][num2 + 1] = mode;
+            map[num1 + 4][num2 + 2] = mode;
+            break;
+        case 3:
+            map[num1][num2] = mode;
+            map[num1][num2 + 1] = mode;
+            map[num1][num2 + 2] = mode;
+            map[num1 + 1][num2 + 2] = mode;
+            map[num1 + 2][num2 + 2] = mode;
+            map[num1 + 2][num2 + 1] = mode;
+            map[num1 + 2][num2] = mode;
+            map[num1 + 3][num2 + 2] = mode;
+            map[num1 + 4][num2 + 2] = mode;
+            map[num1 + 4][num2 + 1] = mode;
+            map[num1 + 4][num2 ] = mode;
+            break;
+        case 4:
+            map[num1][num2] = mode;
+            map[num1][num2 + 2] = mode;
+            map[num1 + 1][num2] = mode;
+            map[num1 + 1][num2 + 2] = mode;
+            map[num1 + 2][num2 + 2] = mode;
+            map[num1 + 2][num2 + 1] = mode;
+            map[num1 + 2][num2] = mode;
+            map[num1 + 3][num2 + 2] = mode;
+            map[num1 + 4][num2 + 2] = mode;
+            break;
+        case 5:
+            map[num1][num2] = mode;
+            map[num1][num2 + 1] = mode;
+            map[num1][num2 + 2] = mode;
+            map[num1 + 1][num2] = mode;
+            map[num1 + 2][num2 + 2] = mode;
+            map[num1 + 2][num2 + 1] = mode;
+            map[num1 + 2][num2] = mode;
+            map[num1 + 3][num2 + 2] = mode;
+            map[num1 + 4][num2] = mode;
+            map[num1 + 4][num2 + 1] = mode;
+            map[num1 + 4][num2 + 2] = mode;
+            break;
+        case 6:
+            map[num1][num2] = mode;
+            map[num1][num2 + 1] = mode;
+            map[num1][num2 + 2] = mode;
+            map[num1 + 1][num2] = mode;
+            map[num1 + 2][num2 + 2] = mode;
+            map[num1 + 2][num2 + 1] = mode;
+            map[num1 + 2][num2] = mode;
+            map[num1 + 3][num2] = mode;
+            map[num1 + 3][num2 + 2] = mode;
+            map[num1 + 4][num2] = mode;
+            map[num1 + 4][num2 + 1] = mode;
+            map[num1 + 4][num2 + 2] = mode;
+            break;
+        case 7:
+            map[num1][num2] = mode;
+            map[num1][num2 + 1] = mode;
+            map[num1][num2 + 2] = mode;
+            map[num1 + 1][num2] = mode;
+            map[num1 + 1][num2 + 2] = mode;
+            map[num1 + 2][num2] = mode;
+            map[num1 + 2][num2 + 2] = mode;
+            map[num1 + 3][num2 + 2] = mode;
+            map[num1 + 4][num2 + 2] = mode;
+            break;
+        case 8:
+            map[num1][num2] = mode;
+            map[num1][num2 + 1] = mode;
+            map[num1][num2 + 2] = mode;
+            map[num1 + 1][num2] = mode;
+            map[num1 + 1][num2 + 2] = mode;
+            map[num1 + 2][num2] = mode;
+            map[num1 + 2][num2 + 1] = mode;
+            map[num1 + 2][num2 + 2] = mode;
+            map[num1 + 3][num2] = mode;
+            map[num1 + 3][num2 + 2] = mode;
+            map[num1 + 4][num2] = mode;
+            map[num1 + 4][num2 + 1] = mode;
+            map[num1 + 4][num2 + 2] = mode;
+            break;
+        case 9:
+            map[num1][num2] = mode;
+            map[num1][num2 + 1] = mode;
+            map[num1][num2 + 2] = mode;
+            map[num1 + 1][num2] = mode;
+            map[num1 + 1][num2 + 2] = mode;
+            map[num1 + 2][num2] = mode;
+            map[num1 + 2][num2 + 1] = mode;
+            map[num1 + 2][num2 + 2] = mode;
+            map[num1 + 3][num2 + 2] = mode;
+            map[num1 + 4][num2] = mode;
+            map[num1 + 4][num2 + 1] = mode;
+            map[num1 + 4][num2 + 2] = mode;
+            break;
+        case 0:
+            map[num1][num2] = mode;
+            map[num1][num2 + 1] = mode;
+            map[num1][num2 + 2] = mode;
+            map[num1 + 1][num2] = mode;
+            map[num1 + 1][num2 + 2] = mode;
+            map[num1 + 2][num2] = mode;
+            map[num1 + 2][num2 + 2] = mode;
+            map[num1 + 3][num2] = mode;
+            map[num1 + 3][num2 + 2] = mode;
+            map[num1 + 4][num2] = mode;
+            map[num1 + 4][num2 + 1] = mode;
+            map[num1 + 4][num2 + 2] = mode;
+            break;
+    }
+}
+
+/*
+void trans(char num) {
+    char datatempa = 0;
+    char datatemp = 0;
+    datatemp = data_moment[7][num];
+    datatempa = data3[7][num];
+    for (char i = 7; i > 0; i--) {
+        datatemp = datatemp * 2 + data_moment[i - 1][num];
+        datatempa = datatempa * 2 + data3[i - 1][num];
+    }
+
+    LATD = datatemp;
+    LATA = datatempa;
+}
+ */
 
 /*char data2_neru[8][16] = {
     {0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
@@ -54,7 +280,7 @@ int checkgit = 460;
     {1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0},
     {0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-};*/
+};
 
 char data3[8][16] = {
     {0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0},
@@ -76,7 +302,7 @@ char data3[8][16] = {
     {0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0},
     {0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-};*/
+};
 
 char data_moment[8][16] = {
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -87,142 +313,4 @@ char data_moment[8][16] = {
     {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0},
     {0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-};
-
-
-char map[18][18];
-
-void setup();
-void mapcopy(char, char);
-void trans(char);
-void makefig(char, char, char, char);
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-    setup();
-
-    for (char i = 0; i < 18; i++) {
-        for (char j = 0; j < 18; j++) {
-            map[i][j] = 0;
-        }
-    }
-
-
-    int count = 0;
-    makefig(0, 8, 2, 1);
-
-
-    while (1) {
-        LATD = 0x00;
-        LATA = 0x00;
-        LATE2 = 0;
-        LATE1 = 1;
-        LATE1 = 0;
-        trans(0);
-        __delay_us(300);
-        LATE2 = 1;
-        for (int i = 0; i < 15; i++) {
-            LATD = 0x00;
-            LATA = 0x00;
-            LATE1 = 1;
-            LATE1 = 0;
-            trans(i + 1);
-            __delay_us(300);
-        }
-    }
-
-    while (1) {
-        LATD = 0x00;
-        LATA = 0x00;
-        LATE2 = 0;
-        LATE1 = 1;
-        LATE1 = 0;
-        mapcopy(1, 2);
-        __delay_us(300);
-        count++;
-        if (count % 150 == 0) {
-            makefig(count / 150 - 1, 8, 2, 0);
-            makefig(count / 150, 8, 2, 1);
-            if (count / 150 > 17)count = 0;
-        }
-        LATE2 = 1;
-        for (char i = 0; i < 15; i++) {
-            LATD = 0x00;
-            LATA = 0x00;
-            LATE1 = 1;
-            LATE1 = 0;
-            mapcopy(1, 2 + i);
-            __delay_us(300);
-        }
-    }
-    return (EXIT_SUCCESS);
-}
-
-void setup() {
-    OSCCON = 0b01110000;
-    ANSELA = 0x00;
-    ANSELB = 0x00;
-    TRISA = 0x00;
-    TRISB = 0x00;
-    TRISC = 0x00;
-    TRISD = 0x00;
-    TRISE = 0x00;
-}
-
-void mapcopy(char num1, char num2) {
-    char datatempa = 0;
-    char datatemp = 0;
-    datatemp = map[num1 + 7][num2];
-    datatempa = map[num1 + 15][num2];
-    for (char i = 7; i > 0; i--) {
-        datatemp = datatemp * 2 + map[num1 + i - 1][num2];
-        datatempa = datatempa * 2 + map[num1 + i + 7][num2];
-    }
-
-    LATD = datatemp;
-    LATA = datatempa;
-}
-
-void trans(char num) {
-    char datatempa = 0;
-    char datatemp = 0;
-    datatemp = data_moment[7][num];
-    datatempa = data3[7][num];
-    for (char i = 7; i > 0; i--) {
-        datatemp = datatemp * 2 + data_moment[i - 1][num];
-        datatempa = datatempa * 2 + data3[i - 1][num];
-    }
-
-    LATD = datatemp;
-    LATA = datatempa;
-}
-
-void makefig(char num1, char num2, char figure, char mode) {
-    if (mode == 1) {
-        map[num1][num2] = 1;
-        map[num1][num2 + 1] = 1;
-        map[num1][num2 + 2] = 1;
-        map[num1 + 1][num2 + 2] = 1;
-        map[num1 + 2][num2 + 2] = 1;
-        map[num1 + 2][num2 + 1] = 1;
-        map[num1 + 2][num2] = 1;
-        map[num1 + 3][num2] = 1;
-        map[num1 + 4][num2] = 1;
-        map[num1 + 4][num2 + 1] = 1;
-        map[num1 + 4][num2 + 2] = 1;
-    } else {
-        map[num1][num2] = 0;
-        map[num1][num2 + 1] = 0;
-        map[num1][num2 + 2] = 0;
-        map[num1 + 1][num2 + 2] = 0;
-        map[num1 + 2][num2 + 2] = 0;
-        map[num1 + 2][num2 + 1] = 0;
-        map[num1 + 2][num2] = 0;
-        map[num1 + 3][num2] = 0;
-        map[num1 + 4][num2] = 0;
-        map[num1 + 4][num2 + 1] = 0;
-        map[num1 + 4][num2 + 2] = 0;
-    }
-}
+};*/
